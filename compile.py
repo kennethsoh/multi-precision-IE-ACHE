ee5e1aaa10a8b223b42d3de22c73380ae848ac8f
kennethsoh <sgguy26@gmail.com>
2020-12-03 16:19:04 +0800

    new feature

--- compile.py
+++ compile.py
@@ -2,7 +2,13 @@
 import os
 import sys
 
-filename = str(input("C File name with extension: "))
-filestriped = filename[:-2]
+#filename = str(input("C File name with extension: "))
+#filestriped = filename[:-2]
 
-os.system(f"g++ {filename} -o {filestriped} -ltfhe-spqlios-fma")
+#os.system(f"g++ {filename} -o {filestriped} -ltfhe-spqlios-fma")
+
+print("Compiling process, alice, cloud and verif files")
+os.system(f"g++ process.c -o process -ltfhe-spqlios-fma")
+os.system(f"g++ alice.c -o alice -ltfhe-spqlios-fma")
+os.system(f"g++ cloud.c -o cloud -ltfhe-spqlios-fma")
+os.system(f"g++ verif.c -o verif -ltfhe-spqlios-fma")
