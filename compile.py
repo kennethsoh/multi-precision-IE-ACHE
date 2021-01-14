4f720f728ffad5e28f6a01bd2fbdca489b62b809
Kenneth Soh <sgguy26@gmail.com>
2021-01-14 21:06:30 +0800

    added -fopenmp flag

--- compile.py
+++ compile.py
@@ -10,5 +10,5 @@ import sys
 print("Compiling process, alice, cloud and verif files")
 os.system(f"g++ process.c -o process -ltfhe-spqlios-fma")
 os.system(f"g++ alice.c -o alice -ltfhe-spqlios-fma")
-os.system(f"g++ cloud.c -o cloud -ltfhe-spqlios-fma")
+os.system(f"g++ cloud.c -o cloud -ltfhe-spqlios-fma" -fopenmp)
 os.system(f"g++ verif.c -o verif -ltfhe-spqlios-fma")
